4af48cd8ba790d6aca84107ebf8239a296931fa4
anais <acabado001@ehu.com>
2025-05-28 19:19:22 +0200

    busqueda por palabra solucionado

--- client/src/pages/publication/publicationList/PublicationList.jsx
+++ client/src/pages/publication/publicationList/PublicationList.jsx
@@ -6,60 +6,47 @@ import SearchFilter from "../../../components/searchFilter/SearchFilter";
 
 import './PublicationList.css';
 
-function PublicationList({ publications = [] }) {
+function PublicationList() {
+    const [publication, setPublication] = useState([]);
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [searchTerm, setSearchTerm] = useState(searchParams.get('search') || '');
+
+    useEffect(() => {
+        handleLoadPublications();
+    }, [])
+
+    const handleLoadPublications = async () => {
+        try {
+            const data = await getAllPublications();
+            setPublication(data);
+        } catch (error) {
+            console.error('Error fetching publications:', error);            
+        }
+    };
+
+    const handleSearchTerm = (newTerm) => {
+        setSearchTerm(newTerm);
+        setSearchParams(params => {
+            params.set("search", newTerm);
+            return params;
+        })
+    };
+
+    const filteredPublications = publication.filter(pub => 
+        pub.title.toLowerCase().includes(searchTerm.toLowerCase())
+    ); /* TODO SEARCH BACK */
+    console.log(filteredPublications)
+
     return (
         <section className="search-publications">
-            <SearchFilter />
+            <SearchFilter onSearch={handleSearchTerm} />
             <div className="publication-list">
-                {publications.map((publication) =>
-                    <PublicationCard publication={publication} key={publication.publication_id} />
-                )}
+            {filteredPublications.map(publication => {
+                return <PublicationCard publication={publication} key={publication.publication_id} />
+            })}
             </div>
         </section>
     )
 }
-
-// function PublicationList() {
-//     const [publication, setPublication] = useState([]);
-//     const [searchParams, setSearchParams] = useSearchParams();
-//     const [searchTerm, setSearchTerm] = useState(searchParams.get('search') || '');
-
-//     useEffect(() => {
-//         handleLoadPublications();
-//     }, [])
-
-//     const handleLoadPublications = async () => {
-//         try {
-//             const data = await getAllPublications();
-//             setPublication(data);
-//         } catch (error) {
-//             console.error('Error fetching publications:', error);            
-//         }
-//     };
-
-//     const handleSearchTerm = (newTerm) => {
-//         setSearchTerm(newTerm);
-//         setSearchParams(params => {
-//             params.set("search", newTerm);
-//             return params;
-//         })
-//     };
-
-//     const filteredPublications = publication.filter(pub => 
-//         pub.title.toLowerCase().includes(searchTerm.toLowerCase())
-//     ); /* TODO SEARCH BACK */
-//     console.log(filteredPublications)
-
-//     return (
-//         <section className="search-publications">
-//             <SearchFilter onSearch={handleSearchTerm} />
-//             <div className="publication-list">
-//             {filteredPublications.map(publication => {
-//                 return <PublicationCard publication={publication} key={publication.publication_id} />
-//             })}
-//             </div>
-//         </section>
-//     )
-// }
 
 export default PublicationList;
